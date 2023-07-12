e6544398dff67fabd6f78ff3db1c61caf6bebe51
asimantobar <126625823+asimantobar@users.noreply.github.com>
2023-07-12 12:13:12 +0200

    Update pot_translations.py

--- pot_translations.py
+++ pot_translations.py
@@ -4,8 +4,6 @@ import os
 import pandas as pd
 from mtranslate import translate
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 path = 'C:/Users/usuario/Desktop/Traducciones'
 
 def process_pot(folder_path):
@@ -17,26 +15,6 @@ def process_pot(folder_path):
             filepath = os.path.join(folder_path, filename)
             names.append(filename) 
             with open(filepath, 'r', encoding='utf-8') as file:
-=======
-path = 'C:/Users/usuario/Downloads/translations'
-=======
-path = 'C:/Users/usuario/Desktop/Traducciones'
->>>>>>> 5b14780 (fix bugs in pot document translations)
-
-def process_pot(folder_path):
-    data_frames = []
-    names = []
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".pot"):
-            filepath = os.path.join(folder_path, filename)
-<<<<<<< HEAD
-            with open(filepath, 'r') as file:
->>>>>>> 6f5a66c (translate to pot files)
-=======
-            names.append(filename) 
-            with open(filepath, 'r', encoding='utf-8') as file:
->>>>>>> 5b14780 (fix bugs in pot document translations)
                 lines = file.readlines()
 
             df_data = []
@@ -50,19 +28,9 @@ def process_pot(folder_path):
             df = df[df['msgid'] != '']
             data_frames.append(df)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     return data_frames, names
 
 
-=======
-    return data_frames
->>>>>>> 6f5a66c (translate to pot files)
-=======
-    return data_frames, names
-
-
->>>>>>> 5b14780 (fix bugs in pot document translations)
 
 def translation(df):
     target_lang = {'Translation_ZH': 'zh-CN'}
@@ -77,8 +45,6 @@ def translation(df):
                 translations.append(None)
 
         df[column] = translations
-<<<<<<< HEAD
-<<<<<<< HEAD
         df.rename(columns={column: 'msgstr'}, inplace=True)
 
 def write_translations(filepath, translations):
@@ -108,49 +74,10 @@ def write_translations(filepath, translations):
 
 
 results, filenames = process_pot(path)
-=======
-=======
-        df.rename(columns={column: 'msgstr'}, inplace=True)
->>>>>>> 5b14780 (fix bugs in pot document translations)
-
-def write_translations(filepath, translations):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-
-    new_lines = []
-    idx = 0
-    while idx < len(lines):
-        line = lines[idx]
-        if line.startswith('msgid'):
-            msgid = line.split('msgid')[1].strip()[1:-1]
-            msgstr = translations.get(msgid)
-            if msgstr is not None:
-                new_lines.append(line)
-                new_lines.append('msgstr "{}"\n'.format(msgstr))
-                idx += 1
-            else:
-                new_lines.append(line)
-        else:
-            new_lines.append(line)
-        idx += 1
-
-<<<<<<< HEAD
-results = process_txt(path)
->>>>>>> 6f5a66c (translate to pot files)
-=======
-    with open(filepath, 'w', encoding='utf-8') as new_file:
-        new_file.writelines(new_lines)
-
-
-
-results, filenames = process_pot(path)
->>>>>>> 5b14780 (fix bugs in pot document translations)
 
 for df in results:
     translation(df)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 cont = 0
 for df in results:
     translations = df.set_index('msgid')['msgstr'].to_dict()
@@ -158,17 +85,3 @@ for df in results:
     write_translations(filepath, translations)
     cont += 1
     
-=======
-for df in results:
-    translations = df.set_index('msgid')['Translation_ZH'].to_dict()
-    write_translations(path, translations)
->>>>>>> 6f5a66c (translate to pot files)
-=======
-cont = 0
-for df in results:
-    translations = df.set_index('msgid')['msgstr'].to_dict()
-    filepath = 'C:/Users/usuario/Desktop/Traducciones/' + filenames[cont]
-    write_translations(filepath, translations)
-    cont += 1
-    
->>>>>>> 5b14780 (fix bugs in pot document translations)
